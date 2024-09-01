4c8a44ce64a1ea7d09218cf58920d6837ea15d63
nara <parknara0213@gmail.com>
2024-09-02 08:02:27 +0900

    아이디 정하기(ing...)

--- src/pages/Auth/EmailSignup/EmailSignup.tsx
+++ src/pages/Auth/EmailSignup/EmailSignup.tsx
@@ -6,6 +6,7 @@ import { LuX } from "react-icons/lu";
 import Stage1 from "./components/Stage1";
 import Stage2 from "./components/Stage2";
 import Stage3 from "./components/Stage3";
+import Stage4 from "./components/Stage4";
 
 interface EmailSignupProps {
   setOpenEmailSignup: React.Dispatch<React.SetStateAction<boolean>>;
@@ -20,7 +21,7 @@ const EmailSignup = ({ setOpenEmailSignup }: EmailSignupProps) => {
     password: "",
   });
 
-  const [stage, setStage] = useState(3);
+  const [stage, setStage] = useState(4);
 
   return (
     <div className="email-signup">
@@ -54,6 +55,13 @@ const EmailSignup = ({ setOpenEmailSignup }: EmailSignupProps) => {
           )}
           {stage === 3 && (
             <Stage3
+              userSignup={userSignup}
+              setUserSignup={setUserSignup}
+              setStage={setStage}
+            />
+          )}
+          {stage === 4 && (
+            <Stage4
               userSignup={userSignup}
               setUserSignup={setUserSignup}
               setStage={setStage}
