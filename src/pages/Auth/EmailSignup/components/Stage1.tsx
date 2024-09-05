a29b5f2da708afbbf28024320e97695d1410c468
nara <parknara0213@gmail.com>
2024-09-05 23:16:30 +0900

    인증 코드 확인 페이지

--- src/pages/Auth/EmailSignup/components/Stage1.tsx
+++ src/pages/Auth/EmailSignup/components/Stage1.tsx
@@ -1,7 +1,9 @@
+import "./stage1.css";
 import { useState } from "react";
 import NormalInput from "../../components/NormalInput";
 import { Stage0Props } from "./Stage0";
 import { UserSignupValidType } from "../../../../types/authTypes";
+import { LuLoader2 } from "react-icons/lu";
 
 interface Stage1Props extends Stage0Props {}
 
@@ -12,6 +14,7 @@ const Stage1 = ({ userSignup, setUserSignup, setStage }: Stage1Props) => {
   });
   const [loading, setLoading] = useState(false);
 
+  const handleResendAuthCode = () => {};
   return (
     <>
       <section className="email-signup-section">
@@ -30,16 +33,24 @@ const Stage1 = ({ userSignup, setUserSignup, setStage }: Stage1Props) => {
           setIsValid={setIsValid}
           setLoading={setLoading}
         />
-        <p className="email-signup-noemail">이메일을 받지 못하셨나요?</p>
+        <p className="email-signup-resend" onClick={handleResendAuthCode}>
+          이메일을 받지 못하셨나요?
+        </p>
       </section>
       <section className="email-signup-section">
         <button
-          className={`email-signup-button${
-            isValid["emailAuth"] ? " valid" : ""
-          }`}
-          onClick={() => {}}
+          className={`email-signup-button valid${loading ? " loading" : ""}`}
+          disabled={loading}
         >
-          다음
+          {loading ? (
+            <LuLoader2
+              className={`email-signup-button-spinner${
+                loading ? " loading" : ""
+              }`}
+            />
+          ) : (
+            "인증 코드 확인"
+          )}
         </button>
       </section>
     </>
