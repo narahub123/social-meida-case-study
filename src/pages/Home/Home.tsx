69beb76c1fbe5dc8ed1bffc42449053a9adf2f96
nara <parknara0213@gmail.com>
2024-09-11 11:43:34 +0900

    홈 탭 리스트

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,16 @@
-import { useEffect } from "react";
+import "./home.css";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const navigate = useNavigate();
+  // 탭 상태 : 추천, 팔로잉 중
+  const [tab, setTab] = useState("following");
+
+  // 탭 변경
+  const handleTab = (tab: string) => {
+    setTab(tab);
+  };
 
   useEffect(() => {
     const baseUrl = import.meta.env.VITE_BASE_URL;
@@ -28,7 +36,38 @@ const Home = () => {
 
     fetchData();
   }, []);
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <div className="home-wrapper">
+        <section className="home-tablist">
+          <div className="home-tablist-wrapper">
+            <div className="home-tablist-item">
+              <p
+                className={`home-tablist-item-text${
+                  tab === "recommend" ? " active" : ""
+                }`}
+                onClick={() => handleTab("recommend")}
+              >
+                추천
+              </p>
+            </div>
+            <div className="home-tablist-item">
+              <p
+                className={`home-tablist-item-text${
+                  tab === "following" ? " active" : ""
+                }`}
+                onClick={() => handleTab("following")}
+              >
+                팔로우 중
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className="home-write">글쓰기</section>
+        <section className="home-postlist">포스트 리스트</section>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
