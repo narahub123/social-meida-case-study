a37d57e745c1fce95a6f19c6b07945576befc76b
nara <parknara0213@gmail.com>
2024-09-16 13:04:04 +0900

    Post : 더보기

--- src/pages/Post/Post.tsx
+++ src/pages/Post/Post.tsx
@@ -5,34 +5,85 @@ import { AiOutlineRetweet } from "react-icons/ai";
 import { FaHeart, FaRegHeart } from "react-icons/fa6";
 import { IoStatsChartSharp } from "react-icons/io5";
 import { useRef, useState } from "react";
-
 import PostMore from "./components/PostMore";
 import useClickOutside from "../../hooks/useClickOutside";
+import { convertDateToYYYYMMDD } from "../../components/Calendar/data/calendar.data";
 
 const Post = () => {
   const containerRef = useRef(null);
+  const imagesRef = useRef<HTMLDivElement>(null);
   // 좋아요 여부
-  const [favorite, setFavorite] = useState(false);
-  // 좋아요 숫자
-  const [favorites, setFavorites] = useState(21);
   // 더보기
   const [openMore, setOpenMore] = useState(false);
+  const [imgNum, setImgNum] = useState(0);
   useClickOutside(containerRef, setOpenMore);
   // 정보
-  const [info, setInfo] = useState<{
-    [key: string]: boolean;
+  const [userInfo, setUserInfo] = useState<{
+    [key: string]: boolean | string;
   }>({
+    username: "하이요",
+    userId: "hihihihi",
     following: false,
     mute: false,
     block: false,
     report: false,
   });
+  const [postInfo, setPostInfo] = useState<{
+    text: string | undefined;
+    images: string[] | undefined;
+    postDate: Date;
+    userId: string;
+    comments: string[];
+    retweets: string[];
+    favorites: string[];
+    views: number;
+  }>({
+    text: "연습",
+    images: ["test/profile1.jpg", "test/profile2.jpg"],
+    postDate: new Date(),
+    userId: "ihih",
+    comments: [],
+    retweets: [],
+    favorites: [],
+    views: 0,
+  });
   // 좋아요 핸들러
   const handleFavoriteUnfavorite = () => {
-    setFavorite(!favorite);
-    if (!favorite) setFavorites((prev) => prev + 1);
-    if (favorite) setFavorites((prev) => prev - 1);
+    if (!postInfo) return;
+
+    if (postInfo.favorites.includes(userInfo.userId as string)) {
+      const newFavs = postInfo.favorites.filter(
+        (fav) => fav !== userInfo.userId
+      );
+      setPostInfo({
+        ...postInfo,
+        favorites: newFavs,
+      });
+    } else {
+      setPostInfo({
+        ...postInfo,
+        favorites: [...postInfo.favorites, userInfo.userId] as string[],
+      });
+    }
   };
+
+  // 사진 dot 핸들러
+  const handleDots = (index: number) => {
+    if (!imagesRef.current) return;
+
+    const width = Math.floor(imagesRef.current.getBoundingClientRect().width);
+
+    console.log(width);
+
+    imagesRef.current.style.setProperty(
+      "--preview-width",
+      `-${width * index}px`
+    );
+
+    setImgNum(index);
+  };
+
+  console.log(postInfo?.favorites);
 
   return (
     <div className="post">
@@ -50,14 +101,16 @@ const Post = () => {
           {/* 헤더 */}
           <div className="post-main-header">
             <div className="post-main-header-left">
-              <p className="post-main-username">이름</p>
+              <p className="post-main-username">{userInfo.username}</p>
 
               <div className="post-main-userid-wrapper">
                 <LuAtSign className="icon" />
-                <p className="post-main-userid">주소</p>
+                <p className="post-main-userid">{userInfo.userId}</p>
               </div>
               <p className="dot" />
-              <p className="post-main-postdate">날짜</p>
+              <p className="post-main-postdate">
+                {convertDateToYYYYMMDD(postInfo.postDate)}
+              </p>
             </div>
             <div className="post-main-header-right" ref={containerRef}>
               <LuMoreHorizontal
@@ -66,8 +119,8 @@ const Post = () => {
               />
               {openMore && (
                 <PostMore
-                  info={info}
-                  setInfo={setInfo}
+                  userInfo={userInfo}
+                  setUserInfo={setUserInfo}
                   setOpenMore={setOpenMore}
                 />
               )}
@@ -75,29 +128,62 @@ const Post = () => {
           </div>
           {/* 메인 */}
           <div className="post-main-content">
-            <div className="post-main-content-text">text</div>
-            <div className="post-main-content-extra">extra</div>
+            <div className="post-main-content-text">{postInfo.text}</div>
+            <div className="post-main-content-extra">
+              <div className="post-main-content-extra-photos-wrapper">
+                <div
+                  className="post-main-content-extra-photos"
+                  ref={imagesRef}
+                  tabIndex={1}
+                >
+                  {postInfo.images?.map((img) => (
+                    <img
+                      src={img}
+                      className="post-main-content-extra-photos-photo"
+                    />
+                  ))}
+                </div>
+                <div className="post-main-content-extra-photos-dots">
+                  {postInfo.images?.map((_, i) => (
+                    <p
+                      className={`dot-indicator${
+                        imgNum === i ? " selected" : ""
+                      }`}
+                      onClick={() => handleDots(i)}
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
           </div>
           {/* 푸터 */}
           <div className="post-main-footer">
             <span className="post-main-footer-item comment">
               <TbMessageCircle className="post-main-footer-item-icon icon" />
-              <p className="post-main-footer-item-text">32</p>
+              <p className="post-main-footer-item-text">
+                {postInfo && postInfo.comments ? postInfo.comments.length : 0}
+              </p>
             </span>
             <span className="post-main-footer-item retweet">
               <AiOutlineRetweet className="post-main-footer-item-icon icon" />
-              <p className="post-main-footer-item-text">32</p>
+              <p className="post-main-footer-item-text">
+                {postInfo && postInfo.retweets ? postInfo.retweets.length : 0}
+              </p>
             </span>
             <span
               className="post-main-footer-item favorite"
               onClick={handleFavoriteUnfavorite}
             >
-              {favorite ? (
+              {postInfo &&
+              userInfo &&
+              postInfo?.favorites.includes(userInfo.userId as string) ? (
                 <FaHeart className="post-main-footer-item-icon icon favorite" />
               ) : (
                 <FaRegHeart className="post-main-footer-item-icon icon unfavorite" />
               )}
-              <p className="post-main-footer-item-text">{favorites}</p>
+              <p className="post-main-footer-item-text">
+                {postInfo && postInfo.favorites ? postInfo.favorites.length : 0}
+              </p>
             </span>
             <span className="post-main-footer-item view">
               <IoStatsChartSharp className="post-main-footer-item-icon icon" />
--- src/pages/Post/components/PostMore.tsx
+++ src/pages/Post/components/PostMore.tsx
@@ -1,4 +1,3 @@
-import { useRef } from "react";
 import "./postMore.css";
 import { GoMute, GoUnmute } from "react-icons/go";
 import { IoStatsChartSharp } from "react-icons/io5";
@@ -11,55 +10,55 @@ import {
   LuUserPlus,
   LuUserX,
 } from "react-icons/lu";
-import useClickOutside from "../../../hooks/useClickOutside";
+
 interface PostMoreProps {
-  info: {
-    [key: string]: boolean;
+  userInfo: {
+    [key: string]: string | boolean;
   };
-  setInfo: React.Dispatch<
+  setUserInfo: React.Dispatch<
     React.SetStateAction<{
-      [key: string]: boolean;
+      [key: string]: string | boolean;
     }>
   >;
   setOpenMore: React.Dispatch<React.SetStateAction<boolean>>;
 }
-const PostMore = ({ info, setInfo, setOpenMore }: PostMoreProps) => {
+const PostMore = ({ userInfo, setUserInfo, setOpenMore }: PostMoreProps) => {
   const handleInfo = (key: string) => {
-    setInfo((prev) => ({
+    setUserInfo((prev) => ({
       ...prev,
-      [key]: !info[key],
+      [key]: !userInfo[key],
     }));
   };
   return (
     <ul className="post-more-modal">
       <li className="post-more-item" onClick={() => handleInfo("following")}>
         <span className="post-more-item-icon icon">
-          {info.following ? <LuUserX /> : <LuUserPlus />}
+          {userInfo.following ? <LuUserX /> : <LuUserPlus />}
         </span>
         <span className="post-more-item-text">
           <LuAtSign className="icon" />
-          <span>주소 님 </span>
-          <span>{info.following ? "언팔로우하기" : "팔로우하기"}</span>
+          <span>{userInfo.userId} 님 </span>
+          <span>{userInfo.following ? "언팔로우하기" : "팔로우하기"}</span>
         </span>
       </li>
       <li className="post-more-item" onClick={() => handleInfo("mute")}>
         <span className="post-more-item-icon">
-          {info.mute ? <GoUnmute /> : <GoMute />}
+          {userInfo.mute ? <GoUnmute /> : <GoMute />}
         </span>
         <span className="post-more-item-text">
           <LuAtSign className="icon" />
-          <span>주소 님 </span>
-          <span>{info.mute ? "언뮤트하기" : "뮤트하기"}</span>
+          <span>{userInfo.userId} 님 </span>
+          <span>{userInfo.mute ? "언뮤트하기" : "뮤트하기"}</span>
         </span>
       </li>
       <li className="post-more-item" onClick={() => handleInfo("block")}>
         <span className="post-more-item-icon">
-          {info.block ? <LuCircle /> : <LuBan />}
+          {userInfo.block ? <LuCircle /> : <LuBan />}
         </span>
         <span className="post-more-item-text">
           <LuAtSign className="icon" />
-          <span>주소 님 </span>
-          <span>{info.block ? "차단풀기" : "차단하기"}</span>
+          <span>{userInfo.userId} 님 </span>
+          <span>{userInfo.block ? "차단풀기" : "차단하기"}</span>
         </span>
       </li>
       <li className="post-more-item">
